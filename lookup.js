508e9428c0d7421fbd0d603f0c0c17b1fe1d0224
brodan <christopher.hranj@gmail.com>
2017-06-20 00:09:36 -0400

    add trim to template literal and refactor message building

--- lookup.js
+++ lookup.js
@@ -3,13 +3,15 @@ const got = require('got');
 exports.handler = function(context, event, callback) {
     let message = "";
     let twiml = new Twilio.twiml.MessagingResponse();
-    got(`https://api.coinmarketcap.com/v1/ticker/${event.Body}/`)
+    got(`https://api.coinmarketcap.com/v1/ticker/${event.Body.trim()}/`)
     .then(response => {
         let result = JSON.parse(response.body)[0];
         let price_change = parseInt(result.percent_change_24h);
-        message = result.symbol + "\t" + result.price_usd +
-            (price_change > 0 ? '\u2191' : '\u2193') + "\t" +
-            Math.abs(result.percent_change_24h);
+        let message_params = [
+            result.symbol, result.price_usd,
+            (price_change > 0 ? '\u2191' : '\u2193'),
+            Math.abs(result.percent_change_24h)];
+        message = message_params.join(" ");
     })
     .catch(error => {
         console.log(error);
